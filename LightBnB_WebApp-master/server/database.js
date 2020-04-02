91ff6968bea66c6973dea0fb50538e4c4577fb85
headhuntar <headhuntar@gmail.com>
2020-04-02 22:03:33 +0000

    added get all reservations function to use db

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -82,8 +82,24 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function (guest_id, limit = 10) {
-  return getAllProperties(null, 2);
-}
+  return pool.query(`
+  SELECT reservations.*, properties.*, AVG(property_reviews.rating) AS average_rating
+  FROM reservations
+  JOIN properties ON properties.id = reservations.property_id
+  JOIN property_reviews ON properties.id = property_reviews.property_id
+  WHERE reservations.guest_id = $1 AND reservations.end_date < Now()::date
+  GROUP BY properties.id, reservations.id
+  ORDER BY reservations.start_date
+  LIMIT $2;
+  `, [guest_id, limit])
+    .then((res) => {
+      if (res.rows.length > 0) {
+        return res.rows;
+      } else {
+        return null;
+      }
+    });
+};
 exports.getAllReservations = getAllReservations;
 
 /// Properties
